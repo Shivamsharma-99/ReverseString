1bc6df451094f98d0ff3e58bc1306a6eedc33816
Shivam Sharma <shivam.piyush99@gmail.com>
2022-02-23 18:24:51 +0530

    Reverse String

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+
+namespace StringProgram
+{
+    class Program
+    {
+        public void reverseString()
+        {
+            // ------------ Method 1. ------ using for loop --------------
+            string name = "Shivam";
+            var chars = name.ToCharArray();
+            var length = chars.Length;
+
+            Console.WriteLine($"String length is : {length}");
+            for (var i = length - 1; i >= 0; i--)
+            {
+                Console.Write(chars[i]);
+            }
+            Console.ReadLine();
+        }
+        public void Revstring()
+        {
+
+            // -------------- Method 2. ---- using Array.Reverse ----------
+            string name1 = "Shivam";
+            var chars1 = name1.ToCharArray();
+            Array.Reverse(chars1);
+            Console.Write(chars1);
+            Console.ReadLine();
+        }
+        public void RevString()
+        {
+            // --------------- Method 3. -------- using String.Reverse -----
+            string name = "Shivam";
+            var revString = new string(name.Reverse().ToArray());
+            Console.WriteLine(revString);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringProgram
+{
+    class stringRev
+    {
+        static void Main(string[] args)
+        {
+            //Program str = new Program();
+            //str.reverseString();
+
+            //Program Str = new Program();
+            //Str.RevString();
+
+            Program mystr = new Program();
+            mystr.Revstring();
+        }
+    }
+}
